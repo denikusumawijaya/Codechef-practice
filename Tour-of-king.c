5d1bcf7b96228df464c6bb9665bb1e549ab4cf15
denikusumawijaya <112800492+denikusumawijaya@users.noreply.github.com>
2022-12-08 12:44:35 +0700

    Create Tour-of-king.c

---
+++ Tour-of-king.c
@@ -0,0 +1,17 @@
+//Problem Code:KNGTOR
+//Contest Code:START67
+
+#include <stdio.h>
+
+int main() {
+    int T;
+    scanf("%d", &T);
+    int N[T], M[T];
+    for (int i=0; i<T; i++) {
+        scanf("\n%d %d", &N[i], &M[i]);
+    }
+    for (int i=0; i<T; i++) {
+        printf("\n%d", ((N[i]*5) + (M[i]*7)));
+    }
+    return 0;
+}
